b661489b0d4086fae2e5769c797f5b95edbf3ada
kanon <vipkia@sina.cn>
2018-07-03 16:19:15 +0800

    update

--- application/configs.py
+++ application/configs.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ws9949021044@127.0.0.1:3306/mine_blog"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/mine_blog"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 JWT_SECRET_KEY = 'jwt_secret'
 JWT_AUTH_URL_RULE = '/api/v1/auth'
