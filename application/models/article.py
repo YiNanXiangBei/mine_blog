3e9e9e2feb7ce16589efde25ea8ed97548a2acd2
kanon <vipkia@sina.cn>
2018-07-03 17:33:38 +0800

    article页面添加查询方法

--- application/models/article.py
+++ application/models/article.py
@@ -16,3 +16,24 @@ class Article(db.Model):
     date_publish = db.Column(db.TIMESTAMP, nullable=False)
     # 在Comment中添加一个属性为article
     comments = db.relationship('Comment', backref='article')
+
+    def __init__(self, tag_id, title, desc, content, click_count, date_publish):
+        self.tag_id = tag_id
+        self.title = title
+        self.desc = desc
+        self.content = content
+        self.click_count = click_count
+        self.date_publish = date_publish
+
+    def __str__(self):
+        return '<Article{}, {}, {}, {}, {}, {}>'. \
+            format(self.id, self.tag_id, self.title, self.desc,
+                   self.click_count, self.date_publish)
+
+    def get_all(self, page_no, page_size=10):
+        articles = db.session.query(Article).filter_by().paginate(page_no, page_size, False)
+        return articles
+
+    def get_by_id(self, id):
+        article = db.session.query(Article).filter_by(id=id)
+        return article
