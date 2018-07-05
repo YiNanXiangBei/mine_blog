72827e0b56ebeba7ddde85445de4020e586382c3
kanon <vipkia@sina.cn>
2018-07-05 17:55:34 +0800

    更新sql

--- application/models/sql/mine_blog.sql
+++ application/models/sql/mine_blog.sql
@@ -37,6 +37,16 @@ CREATE TABLE `blog_article` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `blog_article`
+--
+
+LOCK TABLES `blog_article` WRITE;
+/*!40000 ALTER TABLE `blog_article` DISABLE KEYS */;
+INSERT INTO `blog_article` VALUES (3,'title','desc','content',0,'2018-07-04 11:14:14','1');
+/*!40000 ALTER TABLE `blog_article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `blog_comment`
 --
 
@@ -53,12 +63,21 @@ CREATE TABLE `blog_comment` (
   `comment_id` bigint(20) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`comment_id`),
   UNIQUE KEY `comment_id_UNIQUE` (`comment_id`),
-  KEY `fk_blog_comment_1_idx` (`user_id`),
   KEY `fk_blog_comment_2_idx` (`article_id`),
+  KEY `fk_blog_comment_1_idx` (`user_id`),
   CONSTRAINT `fk_blog_comment_1` FOREIGN KEY (`user_id`) REFERENCES `blog_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_blog_comment_2` FOREIGN KEY (`article_id`) REFERENCES `blog_article` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_comment`
+--
+
+LOCK TABLES `blog_comment` WRITE;
+/*!40000 ALTER TABLE `blog_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `blog_tag`
@@ -77,6 +96,15 @@ CREATE TABLE `blog_tag` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `blog_tag`
+--
+
+LOCK TABLES `blog_tag` WRITE;
+/*!40000 ALTER TABLE `blog_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `blog_user`
 --
 
@@ -84,15 +112,25 @@ DROP TABLE IF EXISTS `blog_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `blog_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表序号',
+  `id` bigint(20) NOT NULL COMMENT '表序号',
   `username` varchar(45) NOT NULL COMMENT '用户名',
-  `link` varchar(50) DEFAULT NULL COMMENT '链接',
+  `link` varchar(200) DEFAULT NULL COMMENT '链接',
   `avatar` varchar(45) CHARACTER SET gb2312 DEFAULT NULL COMMENT '头像',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_user`
+--
+
+LOCK TABLES `blog_user` WRITE;
+/*!40000 ALTER TABLE `blog_user` DISABLE KEYS */;
+INSERT INTO `blog_user` VALUES (1,'test1','www.baidu.com','www.baidu.com');
+/*!40000 ALTER TABLE `blog_user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `system_user`
@@ -113,6 +151,15 @@ CREATE TABLE `system_user` (
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_user`
+--
+
+LOCK TABLES `system_user` WRITE;
+/*!40000 ALTER TABLE `system_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `system_user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -123,4 +170,4 @@ CREATE TABLE `system_user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-04  9:57:10
+-- Dump completed on 2018-07-05  9:53:34
