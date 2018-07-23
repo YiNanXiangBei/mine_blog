28cc0380035752100998ea0ad3b551b81860071e
Lao wang <vipkia@sina.cn>
2018-07-23 22:29:33 +0800

    自动保存功能开发

--- application/configs.py
+++ application/configs.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/mine_blog"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ws9949021044@127.0.0.1:3306/mine_blog"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 JWT_SECRET_KEY = 'jwt_secret'
 JWT_AUTH_URL_RULE = '/api/v1/auth'
@@ -14,8 +14,8 @@ GITHUB_OAUTH = {
     'USER_MESSAGE_PATH': 'https://api.github.com/user',
 }
 TENCENT_OAUTH = {
-    'secret_id': '',
-    'secret_key': '',
-    'region': '',
-    'bucket': ''
+    'secret_id': 'AKIDKUQ7Zn7Y6PSHsEIFuKWTBeuugDWap4Vh',
+    'secret_key': '6ybz82iANL5apoY2HH1WmmaAVbS6VgcJ',
+    'region': 'ap-guangzhou',
+    'bucket': 'mine-blog-1252100560'
 }
