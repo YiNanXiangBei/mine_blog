362b77259bf3fb77142f55f1cf02fa7b00fe40d5
kanon <vipkia@sina.cn>
2018-08-02 14:55:13 +0800

    新增客户端后台

---
+++ application/controllers/client_controller.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+# @author: yinan
+# @time: 18-8-2 下午2:46
+# @filename: client_controller.py
+from flask import Blueprint, request, jsonify
+
+client = Blueprint('/', __name__)
+
