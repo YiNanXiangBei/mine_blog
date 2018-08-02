c35f82c185d7070678c76cf0ecd2ee9dabaf1ab7
kanon <vipkia@sina.cn>
2018-08-02 15:11:51 +0800

    更新数据表以及model

--- application/models/sql/mine_blog.sql
+++ application/models/sql/mine_blog.sql
@@ -30,21 +30,12 @@ CREATE TABLE `blog_article` (
   `click_count` bigint(20) NOT NULL DEFAULT '0' COMMENT '文章点击次数',
   `date_publish` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '发布日期',
   `deleted` varchar(1) NOT NULL DEFAULT '0' COMMENT '是否删除，默认为‘0’代表未删除，‘1’代表已删除',
+  `back_img` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `title_UNIQUE` (`title`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `blog_article`
---
-
-LOCK TABLES `blog_article` WRITE;
-/*!40000 ALTER TABLE `blog_article` DISABLE KEYS */;
-INSERT INTO `blog_article` VALUES (3,'title','desc','content',0,'2018-07-04 11:14:14','1');
-/*!40000 ALTER TABLE `blog_article` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `blog_comment`
@@ -71,15 +62,6 @@ CREATE TABLE `blog_comment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `blog_comment`
---
-
-LOCK TABLES `blog_comment` WRITE;
-/*!40000 ALTER TABLE `blog_comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blog_comment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `blog_tag`
 --
 
@@ -92,17 +74,25 @@ CREATE TABLE `blog_tag` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `tag_UNIQUE` (`tag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `blog_tag`
+-- Table structure for table `blog_tag_article`
 --
 
-LOCK TABLES `blog_tag` WRITE;
-/*!40000 ALTER TABLE `blog_tag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blog_tag` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `blog_tag_article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_tag_article` (
+  `tag_id` bigint(20) NOT NULL COMMENT 'tag外键',
+  `article_id` bigint(20) NOT NULL COMMENT 'article外键',
+  KEY `fk_blog_tag_article_1_idx` (`article_id`),
+  KEY `fk_blog_tag_article_2_idx` (`tag_id`),
+  CONSTRAINT `fk_blog_tag_article_1` FOREIGN KEY (`article_id`) REFERENCES `blog_article` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_blog_tag_article_2` FOREIGN KEY (`tag_id`) REFERENCES `blog_tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `blog_user`
@@ -123,16 +113,6 @@ CREATE TABLE `blog_user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `blog_user`
---
-
-LOCK TABLES `blog_user` WRITE;
-/*!40000 ALTER TABLE `blog_user` DISABLE KEYS */;
-INSERT INTO `blog_user` VALUES (1,'test1','www.baidu.com','www.baidu.com');
-/*!40000 ALTER TABLE `blog_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `system_user`
 --
 
@@ -142,24 +122,15 @@ DROP TABLE IF EXISTS `system_user`;
 CREATE TABLE `system_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表id',
   `username` varchar(45) NOT NULL COMMENT '用户名',
-  `password` varchar(32) NOT NULL COMMENT '用户密码',
+  `password` varchar(100) NOT NULL COMMENT '用户密码',
   `email` varchar(45) NOT NULL COMMENT '用户邮箱',
-  `avatar` varchar(50) DEFAULT NULL COMMENT '头像',
+  `avatar` varchar(100) DEFAULT NULL COMMENT '头像',
   `last_login` timestamp NULL DEFAULT NULL COMMENT '上次登录时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `system_user`
---
-
-LOCK TABLES `system_user` WRITE;
-/*!40000 ALTER TABLE `system_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `system_user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -170,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-05  9:53:34
+-- Dump completed on 2018-08-02  7:10:19
